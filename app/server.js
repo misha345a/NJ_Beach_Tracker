fa79e71105ae5a852b5752bf99e7d646afad989e
Michael Markin <84154105+misha345a@users.noreply.github.com>
2021-08-16 17:03:06 -0400

    Update port info on server.js

--- app/server.js
+++ app/server.js
@@ -2,4 +2,16 @@ const axios = require("axios");
 const express = require('express');
 require('dotenv').config();
 
-let app = express();
+const app = express();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.static("public_html"));
+
+//_________________________________________
+
+app.listen(port, () => {
+    console.log(`Starting server at: ${port}`);
+});

0ad17df9979b58a2c926704395be769f83e555f6
Michael Markin <84154105+misha345a@users.noreply.github.com>
2021-11-10 20:10:49 -0500

    Added convertUnixUTC function

--- server.js
+++ server.js
@@ -10,6 +10,16 @@ app.use(express.static("public_html"));
 
 //_________________________________________
 
+// convert Unix, UTC timestamps into a readable form (i.e. 1575909015000 -> 9 Monday)
+function convertUnixUTC(unixTimestamp) {
+  const milliseconds = unixTimestamp * 1000;
+  const dateObject = new Date(milliseconds);
+  const humanDateFormat = dateObject.toLocaleString("en-US", {day:"numeric", weekday:"long"});
+  return humanDateFormat;
+}
+
+//_________________________________________
+
 app.listen(port, () => {
     console.log(`Starting server at: ${port}`);
 });

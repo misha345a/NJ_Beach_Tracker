438067ca9f223576f989e39c30b73348b13567b2
Michael Markin <84154105+misha345a@users.noreply.github.com>
2021-11-10 20:18:42 -0500

    Added formatting functions

--- server.js
+++ server.js
@@ -18,6 +18,31 @@ function convertUnixUTC(unixTimestamp) {
   return humanDateFormat;
 }
 
+// convert a string to title case
+function toTitleCase(str) {
+  return str.toLowerCase().split(' ').map(function (word) {
+    return (word.charAt(0).toUpperCase() + word.slice(1));
+  }).join(' ');
+}
+
+// format the data before sending it to the client-side
+function adjustFormatting(arr) {
+  arr[2] = Math.round(arr[2])+'°F'; // round feelsLikeMorn
+  arr[3] = Math.round(arr[3])+'°F'; // round feelsLikeDay
+  arr[4] = toTitleCase(arr[4]); // title case description
+  arr[5] = Math.round(arr[5])+'%'; // round percentage of precipitation
+  arr[6] = Math.round(arr[6])+' mph'; // round wind speeds
+  arr[7] = arr[7].toFixed(1); // round UV Index to tenths
+  return arr
+}
+
+// URLs to retrieve forecasts at beach locations
+const url_Brigantine_Beach = `https://api.openweathermap.org/data/2.5/onecall?lat=39.4021&lon=-74.3668&exclude=current,minutely,alerts&units=imperial&appid=${process.env.OPENWEATHER_API_KEY}`;
+const url_Atlantic_City_Beach = `https://api.openweathermap.org/data/2.5/onecall?lat=39.3755&lon=-74.4158&exclude=current,minutely,alerts&units=imperial&appid=${process.env.OPENWEATHER_API_KEY}`;
+const url_Ideal_Beach = `https://api.openweathermap.org/data/2.5/onecall?lat=40.4448&lon=-74.1119&exclude=current,minutely,alerts&units=imperial&appid=${process.env.OPENWEATHER_API_KEY}`;
+const url_Beach_Haven= `https://api.openweathermap.org/data/2.5/onecall?lat=39.5593&lon=-74.2432&exclude=current,minutely,alerts&units=imperial&appid=${process.env.OPENWEATHER_API_KEY}`;
+
+
 //_________________________________________
 
 app.listen(port, () => {
